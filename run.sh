85b77e37011d87a2e8a73a7082e4d645e95dd74b
bkvamme <bkvamme@users.noreply.github.com>
2022-07-13 00:31:48 +0200

    Updated URL for download
    Updated URL for zip download

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 DIR=/var/www/PodcastGenerator
 
 wget -O /tmp/podcastgen.zip \
-    https://github.com/albertobeta/PodcastGenerator/archive/v${PODCASTGEN_VERSION}.zip
+    https://github.com/PodcastGenerator/PodcastGenerator/releases/download/v${PODCASTGEN_VERSION}/PodcastGenerator-v${PODCASTGEN_VERSION}.zip
 
 
 unzip /tmp/podcastgen.zip && \
@@ -13,3 +13,4 @@ unzip /tmp/podcastgen.zip && \
 
 service php7.3-fpm start
 service nginx start && tail -f /var/log/nginx/error.log
+asdasdasd
